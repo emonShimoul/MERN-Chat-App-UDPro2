45025ce4c564daf3c91177fc02944d488cfee261
emonShimoul <emonshimoul@gmail.com>
2025-12-20 23:51:21 +0600

    added some components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
-import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import SettingsPage from './pages/SettingsPage';
+import ProfilePage from './pages/ProfilePage';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -9,6 +14,10 @@ const App = () => {
 
         <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
       </div>
     </>
