6303773a55b0860fb2d70a8c4d835b5e0fb2bdb1
emonShimoul <emonshimoul@gmail.com>
2025-12-20 23:45:36 +0600

    set the route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-      <div className='text-red-500 text-center mt-10'>Hello</div>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+      <div>
+        <Navbar></Navbar>
+
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      </div>
     </>
   )
 }
