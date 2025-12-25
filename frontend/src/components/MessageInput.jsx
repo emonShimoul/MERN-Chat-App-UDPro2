ca0be5c052fed553281081ea816bc906f2155384
emonShimoul <emonshimoul@gmail.com>
2025-12-25 16:21:36 +0600

    create sendMessage function and started MessageInput component design

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { useChatStore } from '../store/useChatStore';
 
 const MessageInput = () => {
+  const [text, setText] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const fileInputRef = useRef(null);
+  const {sendMessage} = useChatStore();
+
+  const handleImageChange = () => {};
+
+  const removeImage = () => {};
+
+  const handleSendMessage = () => {};
+  
   return (
     <div>MessageInput</div>
   )
