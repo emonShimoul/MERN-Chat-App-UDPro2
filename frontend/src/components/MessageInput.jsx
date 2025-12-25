d54294b612527a85b867944387a4a67bc5f2727d
emonShimoul <emonshimoul@gmail.com>
2025-12-26 01:28:34 +0600

    added some MessageInput component design

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -12,10 +12,30 @@ const MessageInput = () => {
   const removeImage = () => {};
 
   const handleSendMessage = () => {};
-  
+
   return (
-    <div>MessageInput</div>
+    <div className="p-4 w-full">
+      {imagePreview && (
+        <div className="mb-3 flex items-center gap-2">
+          <div className="relative">
+            <img
+              src={imagePreview}
+              alt="Preview"
+              className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
+            />
+            <button
+              onClick={removeImage}
+              className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300
+              flex items-center justify-center"
+              type="button"
+            >
+              <X className="size-3" />
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
-export default MessageInput
+export default MessageInput;
