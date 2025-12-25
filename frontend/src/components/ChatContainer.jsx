759d4eb13ee7426a11ca930f4882eceddc58ef16
emonShimoul <emonshimoul@gmail.com>
2025-12-25 15:30:57 +0600

    start designing the chatContainer

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+import { useChartStore } from "../store/useChatStore"
 
 
 const ChatContainer = () => {
+  const {messages, getMessages, isMessagesLoading, selectedUser} = useChartStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  },[selectedUser._id, getMessages]);
+
+  if(isMessagesLoading) return <div>Loading...</div>
+
   return (
     <div>ChatContainer</div>
   )
 }
 
-export default ChatContainer
+export default ChatContainer;
