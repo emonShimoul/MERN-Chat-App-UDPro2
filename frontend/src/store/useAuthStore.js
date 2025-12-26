89d4fa8912a5e1a952f1c6d7aadecd2af3448321
emonShimoul <emonshimoul@gmail.com>
2025-12-27 02:42:17 +0600

    installed and added socket.io functionality in the frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,18 +2,20 @@ import {create} from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 
-export const useAuthStore = create((set) => ({
+export const useAuthStore = create((set, get) => ({
     authUser: null,
     isSigningUp: false,
     isLogging: false,
     isUpdatingProfile: false,
     isCheckingAuth: true,
     onlineUsers: [],
+    socket: null,
 
     checkAuth: async () => {
         try{
             const res = await axiosInstance.get("/auth/check");
             set({authUser: res.data});
+            get().connectSocket();
         } catch (error) {
             console.log("Error in checkAuth:", error);
             set({authUser: null});
@@ -28,6 +30,7 @@ export const useAuthStore = create((set) => ({
             const res = await axiosInstance.post("/auth/signup", data);
             set({authUser: res.data});
             toast.success("Account created successfully!!");
+            get().connectSocket();
         } catch (error) {
             toast.error(error.response.data.message);
         } finally {
@@ -41,6 +44,8 @@ export const useAuthStore = create((set) => ({
             const res = await axiosInstance.post("/auth/login", data);
             set({authUser: res.data});
             toast.success("Logged in successfully");
+
+            get().connectSocket();
         } catch(error){
             toast.error(error.response.data.message);
         } finally{
@@ -53,6 +58,7 @@ export const useAuthStore = create((set) => ({
             await axiosInstance.post("/auth/logout");
             set({authUser: null});
             toast.success("Logged out successfully!!");
+            get().disconnectSocket();
         } catch(error){
             toast.error(error.response.data.message);
         }
@@ -71,4 +77,7 @@ export const useAuthStore = create((set) => ({
             set({isUpdatingProfile: false});
         }
     },
+
+    connectSocket: () => {},
+    disconnectSocket: () => {}
 }))
