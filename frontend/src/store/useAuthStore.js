4a70c44c93a71333c344706c7e55185255bdfa15
emonShimoul <emonshimoul@gmail.com>
2025-12-27 02:57:40 +0600

    run socket.io successfully

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,9 @@
 import {create} from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
+import {io} from "socket.io-client";
+
+const BASE_URL = "http://localhost:5001";
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
@@ -78,6 +81,12 @@ export const useAuthStore = create((set, get) => ({
         }
     },
 
-    connectSocket: () => {},
+    connectSocket: () => {
+        const {authUser} = get();
+        if(!authUser || get().socket?.connected) return;
+
+        const socket = io(BASE_URL);
+        socket.connect();
+    },
     disconnectSocket: () => {}
 }))
